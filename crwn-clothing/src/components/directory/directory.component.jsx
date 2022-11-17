106f57ca692f893d7b511bd5a711e4024e289e49
Affan Ahmad <affana@versa-networks.com>
2022-11-17 21:18:16 +0530

    updating our directory page with directory component and category-item component

---
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CategoryItem from '../category-item/category-item.component'
+import './directory.styles.scss'
+
+const Directory = ({ categories }) => {
+  return (
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  )
+}
+
+export default Directory
