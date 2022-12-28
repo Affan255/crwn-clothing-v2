6c81c382201e08eb685896b9a2a9aa2ea18c600b
Affan Ahmad <affana@versa-networks.com>
2022-12-28 13:21:46 +0530

    Added redux-persist to persist cart items in local storage

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -5,14 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
-import { store } from './store/store';
+import { store, persistedStore } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate persistor={persistedStore}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 
 );
--- crwn-clothing/src/store/store.js
+++ crwn-clothing/src/store/store.js
@@ -1,8 +1,26 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { rootReducer } from './root-reducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const middlewares = [logger]
+const middlewares = [process.env.NODE_ENV !== 'production' && logger]
+    .filter(Boolean)
 
-const composedEnhancers = compose(applyMiddleware(...middlewares))
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const composeEnhancer = (process.env.NODE_ENV !== 'production'
+    && window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares))
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
+
+export const persistedStore = persistStore(store)
